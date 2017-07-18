26ce25296941772c45769f6149515155d5562b90
Ali Khalili <ali.khll.1994@gmail.com>
2017-07-18 20:45:35 +0430

    get info view model added

---
+++ ServiceSampleAndroid/ServiceSampleAndroid/ViewModels/GetInfoViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PropertyChanged;
+using ServiceSampleAndroid.Models;
+using Xamarin.Forms;
+
+namespace ServiceSampleAndroid.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class GetInfoViewModel
+    {
+        public string TelephoneNumber { get; set; }
+
+        public Command SaveIntoDB { get; set; }
+
+        public GetInfoViewModel()
+        {
+            SaveIntoDB = new Command(async () =>
+              {
+                  using (AppDbContext dbContext=new AppDbContext())
+                  {
+                      await dbContext.Database.EnsureDeletedAsync();
+
+                      //return;
+                      await dbContext.Database.EnsureCreatedAsync();
+
+                      await dbContext.Set<User>().AddAsync(new User
+                      {
+                          Name = "ali",
+                          Number = TelephoneNumber
+                      });
+
+                      await dbContext.SaveChangesAsync();
+                  }
+              });
+        }
+    }
+}
