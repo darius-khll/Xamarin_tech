7b967f9ee4d1acc753e9f63524b5e200465753d1
Ali Khalili <ali.khll.1994@gmail.com>
2017-07-18 20:44:54 +0430

    app db context added

---
+++ ServiceSampleAndroid/ServiceSampleAndroid/Models/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Xamarin.Forms;
+
+namespace ServiceSampleAndroid.Models
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(p => p.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbFileName = "SmsSampleApp.db3";
+            string fullDbPath = dbFileName;
+
+            if (Device.OS == TargetPlatform.Android)
+            {
+                fullDbPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "dbFileName");
+            }
+
+            optionsBuilder.UseSqlite($"Filename={fullDbPath}");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
