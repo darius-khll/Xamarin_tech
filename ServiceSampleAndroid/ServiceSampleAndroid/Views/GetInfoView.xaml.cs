3dabb30fcb36f9247627cfacb9f9269f7f61a3f5
Ali Khalili <ali.khll.1994@gmail.com>
2017-07-19 15:14:48 +0430

    getInfo view xaml added

---
+++ ServiceSampleAndroid/ServiceSampleAndroid/Views/GetInfoView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceSampleAndroid.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ServiceSampleAndroid.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GetInfoView : ContentPage
+	{
+		public GetInfoView ()
+		{
+			InitializeComponent ();
+            BindingContext = new GetInfoViewModel();
+		}
+	}
+}
