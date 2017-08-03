e760dca68cd7ef412e757c1cc960b89abd7441d9
Ali Khalili <ali.khll.1994@gmail.com>
2017-08-03 19:52:21 +0430

    home view model added

---
+++ ServiceSampleAndroid/ServiceSampleAndroid/ViewModels/HomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using PropertyChanged;
+using ServiceSampleAndroid.Views;
+using Xamarin.Forms;
+
+namespace ServiceSampleAndroid.ViewModels
+{
+    [AddINotifyPropertyChangedInterface] //Fody
+    public class HomeViewModel
+    {
+        public INavigation Navigation { get; set; }
+
+        public Command GoToInfoPage { get; set; }
+
+        public HomeViewModel()
+        {
+            GoToInfoPage = new Command(async () =>
+            {
+                await Navigation.PushAsync(new GetInfoView());
+            });
+        }
+    }
+}
