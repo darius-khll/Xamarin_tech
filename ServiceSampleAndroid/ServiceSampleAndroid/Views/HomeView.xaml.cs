28748927489d160c9ef7e0ce72035bfbce1a38e9
Ali Khalili <ali.khll.1994@gmail.com>
2017-08-03 19:52:46 +0430

    home xaml added

---
+++ ServiceSampleAndroid/ServiceSampleAndroid/Views/HomeView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceSampleAndroid.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ServiceSampleAndroid.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomeView : ContentPage
+	{
+		public HomeView ()
+		{
+			InitializeComponent ();
+            BindingContext = new HomeViewModel { Navigation = Navigation };
+		}
+	}
+}
